cecce16d1e34acef386788ecb8b2825618de2d5b
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-07-29 17:02:58 -0300

    Initial commit

---
+++ bar.ts
@@ -0,0 +1,6 @@
+// In `foo.ts`, try to import round function from `mathjs` dependency by typing
+// `round`. No auto-import will be suggested.
+//
+// Then uncomment the line below and go back to `foo.ts`. You'll be correctly
+// suggested the auto-import.
+// import { round } from "mathjs";
